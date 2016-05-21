d4035a38367537c2ece9e0b02b7723e9d393227a
tangyfruits <tangyfruits@gmail.com>
2016-05-21 16:49:28 +1200

    Fixed browse and new arrival pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,19 @@
 class PagesController < ApplicationController
   def new_arrivals
+  	 @listings = Listing.all
+    if params[:search]
+      @listings = Listing.search(params[:search]).order("created_at DESC")
+    else
+      @listings = Listing.all.order('created_at DESC')
   end
+end
 
   def browse
+    @listings = Listing.all
+    if params[:search]
+      @listings = Listing.search(params[:search]).order("name DESC")
+    else
+      @listings = Listing.all.order('name DESC')
+    end
   end
 end
