6d4e10bc2f7ec88d35338292cc69a3d3c90e0160
tangyfruits <tangyfruits@gmail.com>
2016-05-21 19:58:33 +1200

    Created a mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,4 +39,16 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "tradechat2016@gmail.com",
+    password: "SCOTT WILLAN PUT IN THE PASSOWRD HERE LOL"
+  }
 end
